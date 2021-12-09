a7a65b60beafcf2c8d05d5ead9580330130a07c3
mmhs2 <m.m.hs260056@ezweb.ne.jp>
2021-12-09 08:25:59 +0000

    bookers2-debug-master-serch

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'searches/search'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -15,4 +16,5 @@ Rails.application.routes.draw do
   	get 'followings' => 'relationships#followings', as: 'followings'
   	get 'followers' => 'relationships#followers', as: 'followers'
   end
+  get '/search', to: 'searches#search'
 end
